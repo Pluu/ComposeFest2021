51d1445943dcf40ae3a3f96e40bfbbcf86ee3a17
pluulove <pluulove@gmail.com>
2021-11-11 02:28:31 +0900

    Step 3

--- week 2-1-Layouts in JetpackCompose/app/src/main/java/com/codelab/layoutsinjetpackcompose/ui/theme/Theme.kt
+++ week 2-1-Layouts in JetpackCompose/app/src/main/java/com/codelab/layoutsinjetpackcompose/ui/theme/Theme.kt
@@ -2,20 +2,22 @@ package com.codelab.layoutsinjetpackcompose.ui.theme
 
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.MaterialTheme.shapes
+import androidx.compose.material.MaterialTheme.typography
 import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
 
 private val DarkColorPalette = darkColors(
-    primary = Purple200,
-    primaryVariant = Purple700,
-    secondary = Teal200
+    primary = purple200,
+    primaryVariant = purple700,
+    secondary = teal200
 )
 
 private val LightColorPalette = lightColors(
-    primary = Purple500,
-    primaryVariant = Purple700,
-    secondary = Teal200
+    primary = purple500,
+    primaryVariant = purple700,
+    secondary = teal200
 
     /* Other default colors to override
     background = Color.White,
@@ -28,9 +30,9 @@ private val LightColorPalette = lightColors(
 )
 
 @Composable
-fun LayoutsInJetpackComposeTheme(
+fun LayoutsCodelabTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
-    content: @Composable() () -> Unit
+    content: @Composable () -> Unit
 ) {
     val colors = if (darkTheme) {
         DarkColorPalette
@@ -40,8 +42,8 @@ fun LayoutsInJetpackComposeTheme(
 
     MaterialTheme(
         colors = colors,
-        typography = Typography,
-        shapes = Shapes,
+        typography = typography,
+        shapes = shapes,
         content = content
     )
 }
